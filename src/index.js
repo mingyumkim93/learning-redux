f1b08569978990d104b8244f969b0649c4666226
MingyumKim <Mingyum93@gmail.com>
2020-05-11 16:52:23 +0300

    simple counter app with redux

--- src/index.js
+++ src/index.js
@@ -1,23 +1,38 @@
+import { createStore } from "redux";
+
 const add = document.getElementById("add");
 const minus = document.getElementById("minus");
 const number = document.getElementById("number");
 
-let count = 0;
+const ADD = "ADD";
+const MINUS = "MINUS";
 
-const updateText = () => {
-  number.innerText = count;
+// reducer is a function that modifies data.
+const reducer = (count = 0, action) => {
+  // whatever it returns, that is the data in my application.
+  switch (action.type){
+    case ADD:
+      return count + 1;
+    case MINUS:
+      return count - 1;
+    default:
+      return count;
+  }
 }
+const countStore = createStore(reducer);
+
+const onChange = () => {
+  number.innerText = countStore.getState();
+}
+countStore.subscribe(onChange);
 
 const handleAdd = () => {
-  count += 1;
-  updateText();
+  countStore.dispatch({type:ADD});
 };
 
 const handleMinus = () => {
-  count -= 1;
-  updateText();
-};
+  countStore.dispatch({type:MINUS});
+}
 
-add.addEventListener("click", handleAdd);
-minus.addEventListener("click", handleMinus);
-
+add.addEventListener("click",handleAdd);
+minus.addEventListener("click",handleMinus);
