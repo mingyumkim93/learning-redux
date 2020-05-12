4c9d7d7c450f6a112edf5784a730c7dff51c95b4
MingyumKim <Mingyum93@gmail.com>
2020-05-12 13:47:49 +0300

    app and detail and home components for react-redux

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import Home from "../routes/Home";
+import Detail from "../routes/Detail";
+
+export default function App(){
+    return(
+        <Router>
+            <Route exact path="/" component={Home}/>
+            <Route path="/:id" component={Detail}/>
+        </Router>
+    )
+}
+
--- src/index.js
+++ src/index.js
@@ -1,55 +1,66 @@
-import { createStore } from "redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
 
-const todoInput = document.getElementById("todo-input");
-const todoButton = document.getElementById("todo-button");
-const todoList = document.getElementById("todo-list");
+ReactDOM.render(<App></App>, document.getElementById("root"));
 
-const ADD = "ADD";
-const DELETE = "DELETE";
 
-const reducer = (state = [], action) => {
-  switch (action.type) {
-    case ADD: {
-      state.push(action.text);
-      return state;
-    }
-    case DELETE: {
-      state.splice(action.id - 1, 1);
-      return state;
-    }
-    default: {
-      return state;
-    }
-  };
-};
 
-const todoStore = createStore(reducer);
 
-const onSubmit = (e) => {
-  e.preventDefault();
-  todoStore.dispatch({ type: ADD, text: todoInput.value });
-  todoInput.value = "";
-}
-todoButton.addEventListener("click", onSubmit);
 
-const deleteTodo = (e) => {
-  todoStore.dispatch({ type: DELETE, id: e.target.parentNode.id });
-};
+// -------------------Vanilla script codes------------------------
+// import { createStore } from "redux";
 
-const onTodosChange = () => {
-  let id = 0;
-  todoList.innerHTML = "";
-  todoStore.getState().forEach(todo => {
-    const todoItem = document.createElement("li");
-    id += 1;
-    todoItem.id = id;
-    const deleteButton = document.createElement("button");
-    deleteButton.innerText = "delete";
-    deleteButton.addEventListener("click", deleteTodo);
-    todoItem.innerText = todo;
-    todoItem.appendChild(deleteButton);
-    todoList.appendChild(todoItem);
-  });
-};
+// const todoInput = document.getElementById("todo-input");
+// const todoButton = document.getElementById("todo-button");
+// const todoList = document.getElementById("todo-list");
 
-todoStore.subscribe(onTodosChange);
+// const ADD = "ADD";
+// const DELETE = "DELETE";
+
+// const reducer = (state = [], action) => {
+//   switch (action.type) {
+//     case ADD: {
+//       state.push(action.text);
+//       return state;
+//     }
+//     case DELETE: {
+//       state.splice(action.id - 1, 1);
+//       return state;
+//     }
+//     default: {
+//       return state;
+//     }
+//   };
+// };
+
+// const todoStore = createStore(reducer);
+
+// const onSubmit = (e) => {
+//   e.preventDefault();
+//   todoStore.dispatch({ type: ADD, text: todoInput.value });
+//   todoInput.value = "";
+// }
+// todoButton.addEventListener("click", onSubmit);
+
+// const deleteTodo = (e) => {
+//   todoStore.dispatch({ type: DELETE, id: e.target.parentNode.id });
+// };
+
+// const onTodosChange = () => {
+//   let id = 0;
+//   todoList.innerHTML = "";
+//   todoStore.getState().forEach(todo => {
+//     const todoItem = document.createElement("li");
+//     id += 1;
+//     todoItem.id = id;
+//     const deleteButton = document.createElement("button");
+//     deleteButton.innerText = "delete";
+//     deleteButton.addEventListener("click", deleteTodo);
+//     todoItem.innerText = todo;
+//     todoItem.appendChild(deleteButton);
+//     todoList.appendChild(todoItem);
+//   });
+// };
+
+// todoStore.subscribe(onTodosChange);
